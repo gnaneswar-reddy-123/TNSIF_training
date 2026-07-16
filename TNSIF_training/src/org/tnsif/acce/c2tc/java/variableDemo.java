76836ac4c2e11de54470f11e975e3cb631c6b791
gnaneshwar-reddy-123 <iswaryanageswari@gmail.com>
2026-07-16 11:55:16 +0530

    First Commit

---
+++ TNSIF_training/src/org/tnsif/acce/c2tc/java/variableDemo.java
@@ -0,0 +1,14 @@
+package org.tnsif.acce.c2tc.java;
+
+public class variableDemo {
+	int price = 10;
+	void print() {
+		String msg = "cambridge";
+		System.out.print(msg);
+	}
+	
+	public static void main(String []args) {
+		variableDemo vr = new variableDemo();
+		vr.print();
+	}
+}
