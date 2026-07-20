22cf781c85fba7405d0ef6d85ab74ec1a845e55b
gnaneshwar-reddy-123 <iswaryanageswari@gmail.com>
2026-07-21 00:04:32 +0530

    first commit

---
+++ TNSIF_training/src/interview2/Arithmetic_Exception.java
@@ -0,0 +1,20 @@
+package interview2;
+
+public class Arithmetic_Exception{
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+
+        try {
+            int result = a / b;
+            System.out.println("Result = " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception: Cannot divide by zero.");
+        }
+
+        System.out.println("Program Ended Successfully.");
+    }
+}
